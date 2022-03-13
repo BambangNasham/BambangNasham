705793665933c7504fa207c090d7908e23258a6d
D0221372_Bambang Nasham <100887317+BambangNasham@users.noreply.github.com>
2022-03-13 23:53:21 +0800

    Add files via upload

---
+++ operator logika.py
@@ -0,0 +1,10 @@
+#operator logika
+#and
+print('hasil dari True and True : ',True and True)
+print('hasil dari False and True :', False and Trur)
+#or
+print('hasil dari True or True : ', True or True)
+print('hasil dari False or True : ', False or True)
+#not
+print('hasil dari not True : ', not True)
+print('hasil dari not False : ', not False)
