a9c4ba0e505c7707b0372136f294931ea4462ec4
D0221372_Bambang Nasham <100887317+BambangNasham@users.noreply.github.com>
2022-03-13 23:57:33 +0800

    Add files via upload

---
+++ penggabungan beberapa tipe data.py
@@ -0,0 +1,16 @@
+#penggabungan beberapa tipe data
+nama = "Bambang"
+print (type(nama))
+umur = 20
+print (type(umur))
+tinggi = 1.65
+print (type(tinggi))
+menikah = True
+print (type(menikah))
+print ("nama :", nama)
+print ("umur : ", umur)
+print (" tinggi :", tinggi)
+if menikah :
+    print ("Belum menikah")
+else:
+    print ("sudah menikah")
