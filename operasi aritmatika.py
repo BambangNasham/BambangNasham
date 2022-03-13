640c6d054da8775062d28bf896d75995aae2620e
D0221372_Bambang Nasham <100887317+BambangNasham@users.noreply.github.com>
2022-03-13 23:51:56 +0800

    Add files via upload

---
+++ operasi aritmatika.py
@@ -0,0 +1,10 @@
+#operator aritmatika
+nilai1 = 8
+nilai2 = 4
+print(nilai1 + nilai2)
+print(nilai1 - nilai2)
+print(nilai1 * nilai2)
+print(nilai1 / nilai2)
+print(nilai1 % nilai2)
+print(nilai1 ** nilai2)
+print(nilai1 // nilai2)
