fea2b4bad9b583b3512dda9bec783542cafc8221
D0221372_Bambang Nasham <100887317+BambangNasham@users.noreply.github.com>
2022-03-13 23:54:01 +0800

    Add files via upload

---
+++ Operator penugasan.py
@@ -0,0 +1,25 @@
+#operator penugasan
+#sama dengan
+nilai1 = 10
+nilai2 = 5
+#tambah sama dengan
+nilai1 += 2
+print(nilai1)
+#kurang sama dengan
+nilai1 -= 2
+print(nilai1)
+#kali sama dengan
+nilai1 *= 2
+print(nilai1)
+#bagi sama dengan
+nilai1 /= 2
+print(nilai1)
+#sisa bagi sama dengan
+nilai1 %= 2
+print(nilai1)
+#pangkat sama dengan
+nilai1 **= 2
+print(nilai1)
+#pembagian bulat sama dengan
+nilai1 //= 2
+print(nilai1)
