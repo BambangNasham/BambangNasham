b230aa9a15c351ca82a44d830a62393caaf3d272
D0221372_Bambang Nasham <100887317+BambangNasham@users.noreply.github.com>
2022-03-20 22:17:14 +0800

    Add files via upload

---
+++ Collection(list,set,tuple,dictonary).py
@@ -0,0 +1,154 @@
+#membuat contoh codingan collectoins
+#list
+#mengisi dengan beberapa value/nila
+
+#hobby = ['sepak bola', 'volly', 'futsal', 'bulu tangkis', ' takraw', 'tennis meja']
+
+#print(hobby)
+
+#menampilkan menggunakan perulangan
+
+#for i in hobby :
+#	print(i)
+	
+#	i = 0
+#	while i < len(hobby) :
+#		print(hobby[i])
+#		i += 1
+		
+#update salah satu value/nilai
+
+#hobby [4] = 'renang'
+#print(hobby)
+
+#hapus salah satu value
+
+#hobby.pop (5)
+#print(hobby)
+
+#tambah value
+
+#hobby.insert(3, 'lari')
+#print(hobby)
+
+#membuat contoh kodingan set
+
+#mengisi dengan beberapa value/nilai
+#nama = {"bambang", 10, False, 5, "pai"}
+
+#menampilkan dengan perulangan
+
+#for i in nama :
+#	print(i)
+
+#nama = list (nama)
+#i = 0
+#while i < len(nama) :
+#	print(nama[i])
+#	i += 1
+	
+#update salah satu nilai
+
+#nama [1] = "nasham"
+#print(nama)
+
+#nama = set(nama)
+
+#hapus salah satu nilai
+
+#nama.remove("nasham")
+#print(nama)
+
+#tambahkan value/nilai
+
+#nama.add("sultan")
+#print(nama)
+
+#membuat kodingan tuple
+
+#mengisi dengan beberapa value/nilai
+
+#hewan = ("kucing", "sapi", "kambing", "ular", "harimau")
+
+#menampilkan dengan perulangan
+
+#for i in hewan :
+#	print(i)
+	
+#a = 0
+#while a < len(hewan):
+#	print(hewan[a])
+#	a += 1
+	
+#hewan = list (hewan)
+
+#update salah satu nilai
+
+#hewan [2] = "burung"
+#print(hewan)
+
+#hapus salah satu nilai
+
+#hewan.pop()
+#print(hewan)
+
+#tambahkan value/nilai
+
+#hewan.append ("singa")
+#print(hewan)
+
+
+
+
+
+
+
+
+#membuat contoh kodingan dictonary
+
+#mengisi dengan beberapa value/nilai
+
+#identitas = {
+#    'nama' : 'bambang',
+#    'umur' : 20,
+#    'asal' : 'mamuju tengah'
+#}
+
+
+#menampilkan dengan perulangan
+
+#for i in identitas.items() :
+#	print(i)
+	
+#for i in identitas :
+#	print(i)
+	
+#for i in identitas.values() :
+#	print(i)
+
+#update salah satu nilai
+
+#identitas ["usia"] = 19
+#print(identitas)
+
+#hapus salah satu nilai
+
+#identitas.pop ('asal')
+#print(identitas)
+
+#tambahkan value/nilai
+
+#identitas ['status'] = 'mahasiswa'
+#print(identitas)
+
+
+
+
+
+
+
+
+
+
+
+
